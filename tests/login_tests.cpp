21df741e3c003a5a219862ac386331332ec0fffe
ebejara <enrique@bejaranomail.com>
2026-01-09 20:10:10 +0100

    remove extern

--- tests/login_tests.cpp
+++ tests/login_tests.cpp
@@ -1,23 +1,48 @@
 #include <gtest/gtest.h>
-#include <selenium/webdriver.h>  // main include - check exact path after cloning
+#include <gmock/gmock.h>
+#include "webdriverxx.h"
 
-using namespace selenium;
+using namespace webdriverxx;
 
-const std::string SELENIUM_URL = "http://localhost:4444";
+const std::string SELENIUM_URL = "http://localhost:4444/wd/hub";
 
 TEST(LoginTests, SuccessfulLogin_G) {
-    WebDriver driver = WebDriver(SELENIUM_URL, Capabilities::Chrome());
+    WebDriver driver = Start(Chrome(), SELENIUM_URL);
 
-    driver.Get("https://www.saucedemo.com/");
+    driver.Navigate("https://www.saucedemo.com/");
 
     driver.FindElement(ById("user-name")).SendKeys("standard_user");
     driver.FindElement(ById("password")).SendKeys("secret_sauce");
     driver.FindElement(ById("login-button")).Click();
 
-    EXPECT_EQ(driver.Title(), "Swag Labs");
+    EXPECT_EQ(driver.GetTitle(), "Swag Labs");
     EXPECT_TRUE(driver.FindElement(ByCss(".inventory_list")).IsDisplayed());
-
-    driver.Quit();
 }
 
-// Repeat similar pattern for InvalidUsername_VG and InvalidPassword_VG
+TEST(LoginTests, InvalidUsername_VG) {
+    WebDriver driver = Start(Chrome(), SELENIUM_URL);
+
+    driver.Navigate("https://www.saucedemo.com/");
+
+    driver.FindElement(ById("user-name")).SendKeys("wrong_user");
+    driver.FindElement(ById("password")).SendKeys("secret_sauce");
+    driver.FindElement(ById("login-button")).Click();
+
+    Element error = driver.FindElement(ByCss("[data-test='error']"));
+    EXPECT_TRUE(error.IsDisplayed());
+    EXPECT_THAT(error.GetText(), ::testing::HasSubstr("Username and password do not match"));
+}
+
+TEST(LoginTests, InvalidPassword_VG) {
+    WebDriver driver = Start(Chrome(), SELENIUM_URL);
+
+    driver.Navigate("https://www.saucedemo.com/");
+
+    driver.FindElement(ById("user-name")).SendKeys("standard_user");
+    driver.FindElement(ById("password")).SendKeys("wrong_password");
+    driver.FindElement(ById("login-button")).Click();
+
+    Element error = driver.FindElement(ByCss("[data-test='error']"));
+    EXPECT_TRUE(error.IsDisplayed());
+    EXPECT_THAT(error.GetText(), ::testing::HasSubstr("Username and password do not match"));
+}
